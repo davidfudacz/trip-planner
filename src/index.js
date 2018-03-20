84968c00485f3b2b6488ad5b09ee0d577bfc2cf5
David Fudacz <davidfudacz@gmail.com>
2018-03-20 10:45:12 -0500

    map integrated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("Hello from JavaScript");
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZGF2aWRmdWRhY3oiLCJhIjoiY2plenRvNXF1MGljbjMzbm5hZms5enplZCJ9.-wKGDmuBwhyZGjjKMaRxgA";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/outdoors-v10" // mapbox has lots of different map styles available.
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'development',
   entry: './src/index.js', // the starting point for our program
   output: {
     path: __dirname + '/public', // the absolute path for the directory where we want the output to be placed
